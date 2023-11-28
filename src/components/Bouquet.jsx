386e3a4aa2a826e811f4950c6f69e1267008f4eb
imPradhyumn <pradhyumn.16jics040@jietjodhpur.ac.in>
2023-11-28 19:18:33 +0530

    Some flowers, leaves removed

--- src/components/Bouquet.jsx
+++ src/components/Bouquet.jsx
@@ -72,31 +72,6 @@ function Bouquet() {
           </div>
         </div>
 
-        <div className="flower flower--2">
-          <div className="flower__leafs flower__leafs--2">
-            <div className="flower__leaf flower__leaf--1"></div>
-            <div className="flower__leaf flower__leaf--2"></div>
-            <div className="flower__leaf flower__leaf--3"></div>
-            <div className="flower__leaf flower__leaf--4"></div>
-            <div className="flower__white-circle"></div>
-
-            <div className="flower__light flower__light--1"></div>
-            <div className="flower__light flower__light--2"></div>
-            <div className="flower__light flower__light--3"></div>
-            <div className="flower__light flower__light--4"></div>
-            <div className="flower__light flower__light--5"></div>
-            <div className="flower__light flower__light--6"></div>
-            <div className="flower__light flower__light--7"></div>
-            <div className="flower__light flower__light--8"></div>
-          </div>
-          <div className="flower__line">
-            <div className="flower__line__leaf flower__line__leaf--1"></div>
-            <div className="flower__line__leaf flower__line__leaf--2"></div>
-            <div className="flower__line__leaf flower__line__leaf--3"></div>
-            <div className="flower__line__leaf flower__line__leaf--4"></div>
-          </div>
-        </div>
-
         <div className="flower flower--3">
           <div className="flower__leafs flower__leafs--3">
             <div className="flower__leaf flower__leaf--1"></div>
@@ -126,22 +101,6 @@ function Bouquet() {
           <div className="flower__g-long">
             <div className="flower__g-long__top"></div>
             <div className="flower__g-long__bottom"></div>
-          </div>
-        </div>
-
-        <div className="growing-grass">
-          <div className="flower__grass flower__grass--1">
-            <div className="flower__grass--top"></div>
-            <div className="flower__grass--bottom"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--1"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--2"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--3"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--4"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--5"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--6"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--7"></div>
-            <div className="flower__grass__leaf flower__grass__leaf--8"></div>
-            <div className="flower__grass__overlay"></div>
           </div>
         </div>
 
@@ -217,21 +176,6 @@ function Bouquet() {
           </div>
         </div>
 
-        <div className="long-g long-g--0">
-          <div className="grow-ans">
-            <div className="leaf leaf--0"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
         <div className="long-g long-g--1">
           <div className="grow-ans">
             <div className="leaf leaf--0"></div>
@@ -278,51 +222,6 @@ function Bouquet() {
         </div>
 
         <div className="long-g long-g--4">
-          <div className="grow-ans">
-            <div className="leaf leaf--0"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
-        <div className="long-g long-g--5">
-          <div className="grow-ans">
-            <div className="leaf leaf--0"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
-        <div className="long-g long-g--6">
-          <div className="grow-ans">
-            <div className="leaf leaf--0"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--1"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--2"></div>
-          </div>
-          <div className="grow-ans">
-            <div className="leaf leaf--3"></div>
-          </div>
-        </div>
-
-        <div className="long-g long-g--7">
           <div className="grow-ans">
             <div className="leaf leaf--0"></div>
           </div>
